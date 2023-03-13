15a6c3a5f032d208f4cc049b6f6752f7e324d5e9
d00234340 <d00234340@student.dkit.ie>
2023-03-13 11:47:02 +0000

    adds a StackFullException and modifies the BoundedStacks

--- Sticks/src/ToDoList2.java
+++ Sticks/src/ToDoList2.java
@@ -1,12 +1,16 @@
 public class ToDoList2 {
     public static void main(String[] args) {
-        BoundedStacks x = new BoundedStacks(20);
+        BoundedStacks x = new BoundedStacks(4);
         x.push("1");
         x.push("2");
         x.push("3");
         x.push("4");
         x.push("5");
         x.push("6");
+        x.push("7");
+        x.push("8");
+        x.push("9");
+        x.push("10000");
         x.push("7");
         x.push("8");
         x.push("9");
